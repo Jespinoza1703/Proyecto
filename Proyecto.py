1e0082ca06572df7c4147aae7201a88079878e48
Jessica <[jessicaesp2000@hotmail.com]>
2018-05-08 17:20:30 -0600

    "Agregué variables de color"

--- Proyecto.py
+++ Proyecto.py
@@ -1,2 +1,17 @@
-print("hola")
-print("hola2")
+import pygame as pygame
+import time as time
+pygame.init()
+
+ancho = 900
+largo = 700
+
+negro = (0, 0, 0)
+blanco = (255, 255, 255)
+azul = (0,0,255)
+rojo = (255,0,0)
+verde = (0,255,0)
+rojo1 = (150, 0, 0)
+verde1 = (0,150,0)
+azul1 = (0,0,150)
+
+font = pygame.font.SysFont ("Arial", 28)
