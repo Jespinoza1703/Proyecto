b4e1d164f4aa64c951b67b7f7df46bb4ec938c41
Jessica <[jessicaesp2000@hotmail.com]>
2018-05-08 17:26:50 -0600

    "Clase Figuras"

--- Proyecto.py
+++ Proyecto.py
@@ -13,3 +13,24 @@ verde = (0,255,0)
 rojo1 = (150, 0, 0)
 verde1 = (0,150,0)
 azul1 = (0,0,150)
+
+
+gameDisplay = pygame.display.set_mode((ancho, largo)) #tamaño de la ventana
+pygame.display.set_caption("Borrador") #titulo
+clock = pygame.time.Clock()
+
+
+class Figura():
+	def __init__(self,x_fig,y_fig,ancho_fig,largo_fig,color_fig=blanco):#,color):
+
+		self.__ancho_fig = ancho_fig
+		self.__largo_fig = largo_fig
+		self.__color_fig = color_fig
+
+		self.image = pygame.Surface((self.__ancho_fig,self.__largo_fig))
+		self.image.fill(self.__color_fig)
+		self.rect = self.image.get_rect()
+		self.rect.x = x_fig
+		self.rect.y = y_fig
+
+
