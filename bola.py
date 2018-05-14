38a4ae7a550e35c9b3b377eed9710a4dacde6bb3
Alejandro I <josealejandroibarra@hotmail.com>
2018-05-14 11:59:15 -0600

    bola rebota con todos los bordes a ambas direcciones

--- bola.py
+++ bola.py
@@ -23,7 +23,7 @@ class Bola:
 
 	def mover(self, matriz):
 		# se mueve primero en las filas y luego en las columnas
-		matriz[self.y][self.x] = 0
+		matriz[self.y][self.x] = 0 #poner en negro el cuadro anterior
 
 
 		if self.y == 23 and self.direction == -1: #cuando llegua abajo, cambie de direccion hacia arriba
@@ -31,60 +31,63 @@ class Bola:
 		elif self.y == 1 and self.direction == 1: #cuando llegue arriba, cambie de direccion hacia abajo
 			self.direction = -1
 
-		elif self.x == 38:
+		elif self.x == 38: #si llega al borde derecho, cambie de direccion hacia izquierda
 			self.right = False
-		elif self.right == False and self.x < 39:
-			self.right = False
-		elif self.right == True and self.x < 39:
-			self.right = True
+		elif self.x == 1:
+			self.right = True #si llega al borde izquierdi, cambie de direccion hacia derecha
 
 		if self.right == True: #si right es true, o sea, hacia la derecha
-			if self.x < 38 and self.direction == -1:
+			if self.direction == -1:
+				self.x += self.vx
+				self.y += self.vy
+				matriz[self.y][self.x] = 1
 				print(1)
-				self.x += self.vx
-				self.y += self.vy
-				matriz[self.y][self.x] = 1
 				print('direction: '+str(self.direction))
 				print('right: '+str(self.right))
 				print('x: '+str(self.x))
 				print('y: '+str(self.y))
-				#elif self.x == 24 or self.y < 39:
-					#print(3)
-					#self.x += self.vx
-					#self.y += self.vy
-					#matriz[self.y][self.x] = 1
-			elif self.x < 38 and self.direction == 1:
+			elif self.direction == 1:
+				self.x += self.vx
+				self.y -= self.vy 
 				print(2)
-				self.x += self.vx
-				self.y -= self.vy 
 				print('direction: '+str(self.direction))
 				print('right: '+str(self.right))
 				print('x: '+str(self.x))
 				print('y: '+str(self.y))
 				matriz[self.y][self.x] = 1
-			elif self.x < 38 and self.direction == 0:
+			elif self.direction == 0:
+				self.x += self.vx
+				matriz[self.y][self.x] = 1
 				print(3)
-				self.x += self.vx
 				print('direction: '+str(self.direction))
 				print('right: '+str(self.right))
 				print('x: '+str(self.x))
 				print('y: '+str(self.y))
-				matriz[self.y][self.x] = 1
 
-		elif self.right == False: #si right es false, o sea hacia la izquierda
-			print(0, self.x, self.y, self.direction)
-			if self.x != 39 and self.y != 24 and self.direction == True:
-				print(4)
+		if self.right == False: #si right es false, o sea hacia la izquierda
+			if self.direction == -1:
 				self.x -= self.vx
 				self.y += self.vy
 				matriz[self.y][self.x] = 1
-				print(self.direction)
+				print(4)
+				print('direction: '+str(self.direction))
+				print('right: '+str(self.right))
 				print('x: '+str(self.x))
 				print('y: '+str(self.y))
-			elif self.x != 39 and self.y != 1 and self.direction == False:
-				print(5)
+			elif self.direction == 1:
 				self.x -= self.vx
 				self.y -= self.vy 
+				print(5)
+				print('direction: '+str(self.direction))
+				print('right: '+str(self.right))
 				print('x: '+str(self.x))
 				print('y: '+str(self.y))
 				matriz[self.y][self.x] = 1
+			elif self.direction == 0:
+				self.x -= self.vx
+				matriz[self.y][self.x] = 1
+				print(6)
+				print('direction: '+str(self.direction))
+				print('right: '+str(self.right))
+				print('x: '+str(self.x))
+				print('y: '+str(self.y))
