149890262185bf6d5e26f80772881c20f6c4241b
Jessica <[jessicaesp2000@hotmail.com]>
2018-05-14 05:47:07 -0600

    time.sleep()

--- bola.py
+++ bola.py
@@ -19,7 +19,15 @@ class Bola:
 
 	def mover(self, matriz):
 		# se mueve primero en las filas y luego en las columnas
+
 		matriz[self.y][self.x] = 0
-		self.x += self.vx
-		self.y += self.vy
-		matriz[self.y][self.x] = 1
+		if self.x < 24 and self.y < 39:
+			print(1)
+			self.x += self.vx
+			self.y += self.vy
+			matriz[self.y][self.x] = 1
+		else:
+			print(2)
+			self.x -= self.vx
+			self.y -= self.vy
+			matriz[self.y][self.x] = 1
