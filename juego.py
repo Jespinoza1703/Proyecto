188c7d9cce79533acabff0faa1e942302db5a20a
Jessica <[jessicaesp2000@hotmail.com]>
2018-05-14 03:41:51 -0600

    dibujarMatriz

--- juego.py
+++ juego.py
@@ -20,16 +20,25 @@ ANCHO = 780
 LARGO = 480
 
 class Juego:
-    def __init__(self):
-        pygame.init()
-        self.pantalla = pygame.display.set_mode((ANCHO,LARGO))
-        pygame.display.set_caption("PONG")
+	def __init__(self):
+		pygame.init()
+		self.pantalla = pygame.display.set_mode((ANCHO,LARGO))
+		pygame.display.set_caption("PONG")
 
-        
-    def crearMatriz(self):
-        for i in range(self.FILAS):
-            fila = []
-            for j in range(self.COLUMNAS):
-                # Llena la lista de ceros
-                fila.append(0)
-            self.matriz.append(fila)
+
+	def crearMatriz(self):
+		for i in range(self.FILAS):
+			fila = []
+			for j in range(self.COLUMNAS):
+				# Llena la lista de ceros
+				fila.append(0)
+			self.matriz.append(fila)
+
+	def dibujarMatriz(self):
+		for fila in range(self.FILAS):
+			for columna in range(self.COLUMNAS):
+				if self.matriz[fila][columna] == 0:
+					pygame.draw.rect(self.pantalla, BLACK, [L* columna,L * fila,L,L])
+				else:
+					pygame.draw.rect(self.pantalla, WHITE, [L* columna,L * fila,L,L])
+		pygame.draw.line(self.pantalla, WHITE, [400, 0], [400,600], 4)
