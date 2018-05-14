fa44b9e97a92f75fa70b2f3ddf7358dcae3a93a6
Jessica <[jessicaesp2000@hotmail.com]>
2018-05-14 03:35:20 -0600

    clase barra

--- barra.py
+++ barra.py
@@ -1,1 +1,26 @@
+# Clase Barra:
+# Atributos:
+# x
+# y
+# ancho
+# largo
+# vel_y
+#######################
+# Metodos:
+# mover_barra()
 
+class Barra:
+    def __init__(self,x,y,ancho,largo):
+        self.ancho = ancho
+        self.largo = largo
+        self.x = x
+        self.y =y
+
+    def posicionar_barra(self):
+        self.ancho 
+        self.largo
+
+    def mover(self,vy,matriz):
+        matriz[self.y][self.x] = 0
+        self.y += vy
+        matriz[self.y][self.x] = 1
