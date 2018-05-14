7a7a8b5c5f48250b534ddf531b16baad957a2c04
Jessica <[jessicaesp2000@hotmail.com]>
2018-05-14 17:51:53 -0600

    cambios en mover

--- barra.py
+++ barra.py
@@ -10,17 +10,29 @@
 # mover_barra()
 
 class Barra:
-    def __init__(self,x,y,largo):
-        self.ancho = 1
-        self.largo = largo
-        self.x = x
-        self.y =y
+	def __init__(self,x,y,largo):
+		self.ancho = 1
+		self.largo = largo
+		self.x = x
+		self.y = y
+		self.up = False
+		self.vy = 1
 
-    def posicionar(self,matriz):
-        for n in range(self.largo):
-        	matriz[self.y+n][self.x] = 1
+	def posicionar(self,matriz):
+		for n in range(self.largo):
+			matriz[self.y+n][self.x] = 1
 
-    def mover(self,vy,matriz):
-        matriz[self.y][self.x] = 0
-        self.y += vy
-        matriz[self.y][self.x] = 1
+	def mover(self,matriz,direccion):
+		matriz[self.y][self.x] = 0
+		if self.y == 24: #si llega al borde derecho, cambie de direccion hacia izquierda
+			self.up = False
+		elif self.y == 0:
+			self.up = True #si llega al borde izquierdi, cambie de direccion hacia derecha
+
+		if self.up == True: #si up es true, o sea, hacia la derecha
+			if self.direccion == -1:
+				self.y += self.vy
+				matriz[self.y][self.x] = 1
+			elif self.direccion == 1:
+				self.y -= self.vy 
+				matriz[self.y][self.x] = 1
