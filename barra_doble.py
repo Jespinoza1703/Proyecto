10dd7d58f17862638e514d85e311afc7c80a3a26
Jessica <[jessicaesp2000@hotmail.com]>
2018-05-15 17:28:38 -0600

    Se coloca la segunda barra

--- barra_doble.py
+++ barra_doble.py
@@ -1,37 +1,37 @@
-class Barra_doble:
-    def __init__(self,x,y,largo):
-        self.ancho = 1
-        self.largo = largo
-        self.x = x
-        self.y =y
-        self.paleta_doble_posicion = [[],[]]
-    def posicionar(self,matriz):
-        for n in range(self.largo):
-            self.paleta_doble_posicion[0].append((self.x-self.largo-1)+n)
-        for n in range(self.largo):
-            self.paleta_doble_posicion[1].append((self.x+1)+n)
-        for posicion in self.paleta_doble_posicion[0]:
-            matriz[posicion][self.y] = 1
-        for posicion in self.paleta_doble_posicion[1]:
-            matriz[posicion][self.y] = 1
-    def mover(self,movimiento,matriz):
-        n = len(self.paleta_doble_posicion[0])
-        if movimiento == -1:
-            if self.paleta_doble_posicion[0][0] > 0:
-                matriz[self.paleta_doble_posicion[0][n-1]][self.y] = 0
-                matriz[self.paleta_doble_posicion[1][n-1]][self.y] = 0
-                for i in range(n):
-                    self.paleta_doble_posicion[0][i] += movimiento
-                    self.paleta_doble_posicion[1][i] += movimiento
-                    matriz[self.paleta_doble_posicion[0][i]][self.y] = 1
-                    matriz[self.paleta_doble_posicion[1][i]][self.y] = 1
-        else:
-            if self.paleta_doble_posicion[1][n-1] < 24:
-                matriz[self.paleta_doble_posicion[0][0]][self.y] = 0
-                matriz[self.paleta_doble_posicion[1][0]][self.y] = 0
-                for i in range(n):
-                    self.paleta_doble_posicion[0][i] += movimiento
-                    self.paleta_doble_posicion[1][i] += movimiento
-                    matriz[self.paleta_doble_posicion[0][i]][self.y] = 1
-                    matriz[self.paleta_doble_posicion[1][i]][self.y] = 1
+# Clase Barra:
+# Atributos:
+# x1
+# y1
+# x2
+# y2
+# ancho
+# largo
+# vel_y
+#######################
+# Metodos:
+# mover_barra()
 
+class Barra_doble:
+	def __init__(self,x1,y1,x2,y2,largo):
+		self.ancho = 1
+		self.largo = largo
+		self.x1 = x1
+		self.y1 = y1
+		self.x2 = x2
+		self.y2 = y2
+	# Coloca la barra en la matriz
+	def posicionar(self,matriz):
+		for n in range(self.largo):
+			matriz[self.y1+n][self.x1] = 1
+		for n in range(self.largo):
+			matriz[self.y2+n][self.x2] = 1
+	# Permite mover la barra sin que mueva a través de la matriz
+	def mover(self,vy,matriz):
+		if vy == -1 and self.y1 + self.largo < 25:
+			matriz[self.y1][self.x1] = 0
+			self.y1 -= vy
+			matriz[self.y1][self.x1] = 1
+		elif vy == 1 and self.y1 > 0:
+			matriz[self.y1 + self.largo - 1][self.x1] = 0
+			self.y1 -= vy
+			matriz[self.y1][self.x1] = 1
