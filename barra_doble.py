db324c12fb522db80a995354daefb41f1ae46444
Jessica <[jessicaesp2000@hotmail.com]>
2018-05-16 00:52:19 -0600

    arreglo restriccion

--- barra_doble.py
+++ barra_doble.py
@@ -34,7 +34,7 @@ class Barra_doble:
 			matriz[self.y2+n][self.x2] = 1
 	# Permite mover la barra sin que mueva a través de la matriz
 	def mover(self,vy,matriz):
-		if vy == -1 and self.y1 + self.largo < 25 and self.y2 - self.largo < 25:
+		if vy == -1 and self.y1 + self.largo < 25 and self.y2 + self.largo < 25:
 			matriz[self.y1][self.x1] = 0
 			matriz[self.y2][self.x2] = 0
 			self.y1 -= vy
