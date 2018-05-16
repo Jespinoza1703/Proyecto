25a2e5d97ad2af47822282c3f3fe12db29f6d15d
Alejandro I <josealejandroibarra@hotmail.com>
2018-05-16 10:18:55 -0600

    agregado imagenes de interfaz

--- MenuTkinter.py
+++ MenuTkinter.py
@@ -54,22 +54,6 @@ def Double_nivel3():
 		juego3 = Juego("Double", 3)
 		juego3.jugar()
 
-
-boton = Button (window, text = "Single_Nivel1", font = ("arial", 12), width = 12, command = Single_nivel1)
-boton.place (x = 100, y = 100)
-boton2 = Button (window, text = "Single_Nivel2", font = ("arial", 12), width = 12, command = Single_nivel2)
-boton2.place (x = 250, y = 100)
-boton3 = Button (window, text = "Single_Nivel3", font = ("arial", 12), width = 12, command = Single_nivel3)
-boton3.place (x = 400, y = 100)
-boton4 = Button (window, text = "Double_Nivel1", font = ("arial", 12), width = 12, command = Double_nivel1)
-boton4.place (x = 100, y = 200)
-boton5 = Button (window, text = "Double_Nivel2", font = ("arial", 12), width = 12, command = Double_nivel2)
-boton5.place (x = 250, y = 200)
-boton6 = Button (window, text = "Double_Nivel3", font = ("arial", 12), width = 12, command = Double_nivel3)
-boton6.place (x = 400, y = 200)
-
-
-
 def Single_nivel1():
 	if __name__ == "__main__":#cuando le diga ejecutar, que llame primero al condicional Pong, es lo primero que se va a ejecutar
 		juego = Juego("Single", 1)
@@ -100,9 +84,22 @@ def Double_nivel3():
 		juego3 = Juego("Double", 3)
 		juego3.jugar()
 
-#label de logo principal
+#labels
 mainlogo_label = Label(window, image = mainlogo)
 mainlogo_label.place(x = 30, y = 45)
+oneplayer_label = Label(window, text = '1 Racket AI', font = 'Helvetica 24')
+oneplayer_label.place(x = 315, y = 195)
+oneplayertworacket_label = Label(window, text = '2 Rackets AI', font = 'Helvetica 24')
+oneplayertworacket_label.place(x = 585, y = 195)
+twoplayer_label = Label(window, text = '1 Racket 2P', font = 'Helvetica 24')
+twoplayer_label.place(x = 320, y = 370)
+twoplayertworacket_label = Label(window, text = '2 Rackets 2P', font = 'Helvetica 24')
+twoplayertworacket_label.place(x = 585, y = 370)
+about_label = Label(window, text = 'About', font = 'Helvetica 24')
+about_label.place(x = 130, y = 410)
+help_label = Label(window, text = 'How to play', font = 'Helvetica 24')
+help_label.place(x = 130, y = 310)
+
 
 #botones de inicio de juego
 oneplayer_button = Button(window, image = racket1cpuicon, command = Single_nivel1)
@@ -119,15 +116,15 @@ help_button = Button(window, image = helpicon)
 help_button.place(x = 50, y = 300)
 
 boton2 = Button (window, text = "Single_Nivel2", font = ("arial", 12), width = 12, command = Single_nivel2)
-boton2.place (x = 250, y = 100)
+boton2.place (x = 250, y = 400)
 boton3 = Button (window, text = "Single_Nivel3", font = ("arial", 12), width = 12, command = Single_nivel3)
-boton3.place (x = 400, y = 100)
+boton3.place (x = 400, y = 400)
 boton4 = Button (window, text = "Double_Nivel1", font = ("arial", 12), width = 12, command = Double_nivel1)
-boton4.place (x = 100, y = 200)
+boton4.place (x = 100, y = 450)
 boton5 = Button (window, text = "Double_Nivel2", font = ("arial", 12), width = 12, command = Double_nivel2)
-boton5.place (x = 250, y = 200)
+boton5.place (x = 250, y = 450)
 boton6 = Button (window, text = "Double_Nivel3", font = ("arial", 12), width = 12, command = Double_nivel3)
-boton6.place (x = 400, y = 200)
+boton6.place (x = 400, y = 450)
 
 
 mainloop()
