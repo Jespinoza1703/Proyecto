8fd274ac8a3d4371e65f5af846b74b00e03b0345
Jessica <[jessicaesp2000@hotmail.com]>
2018-05-16 02:19:29 -0600

    funciones get

--- barra_doble.py
+++ barra_doble.py
@@ -20,18 +20,25 @@ class Barra_doble:
 		self.x2 = x2
 		self.y2 = y2
 
-	def get_y1(self):
-		return self.y1
-
-	def get_y2(self):
-		return self.y2
-
 	# Coloca la barra en la matriz
 	def posicionar(self,matriz):
 		for n in range(self.largo):
 			matriz[self.y1+n][self.x1] = 1
 		for n in range(self.largo):
 			matriz[self.y2+n][self.x2] = 1
+
+	def get_x1(self):
+		return self.x1
+
+	def get_y(self):
+		return self.y1
+
+	def get_x2(self):
+		return self.x2
+
+	def get_y2(self):
+		return self.y2
+
 	# Permite mover la barra sin que mueva a través de la matriz
 	def mover(self,vy,matriz):
 		if vy == -1 and self.y1 + self.largo < 25 and self.y2 - self.largo < 25:
