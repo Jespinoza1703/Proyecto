86af658f464a69f71383b6a6af0c4b3a4cc9603d
Alejandro I <josealejandroibarra@hotmail.com>
2018-05-16 21:34:01 -0600

    elimine algunas lineas

--- MenuTkinter.py
+++ MenuTkinter.py
@@ -6,12 +6,6 @@
 
 from juego import *
 from tkinter import *
-from threading import Thread
-
-#variables globales
-animate = False
-xspeed = 3
-yspeed = 4
 
 window = Tk ( ) #hace la ventana
 window.title ("Pong") #le pone el título a la ventana
@@ -43,15 +37,14 @@ abigail = loadPicture('abigail.gif')
 alejandro = loadPicture('alejandro.gif')
 jessica = loadPicture('jessica.gif')
 
-def Single_nivel1():
+def Single_humano():
 	if __name__ == "__main__":#cuando le diga ejecutar, que llame primero al condicional Pong, es lo primero que se va a ejecutar
-		juego = Juego("Single", 1)
+		juego = Juego("Single", 1, "humano")
 		juego.jugar()
 
-def Single_nivel2():
-	window.destroy()
+def Single_cpu():
 	if __name__ == "__main__":#cuando le diga ejecutar, que llame primero al condicional Pong, es lo primero que se va a ejecutar
-		juego2 = Juego("Single", 2)
+		juego2 = Juego("Single", 1, "cpu")
 		juego2.jugar()
 
 def Single_nivel3():
@@ -89,20 +82,16 @@ def Single_nivel3():
 		juego3 = Juego("Single", 3)
 		juego3.jugar()
 
-def Double_nivel1():
+def Double_humano():
 	if __name__ == "__main__":#cuando le diga ejecutar, que llame primero al condicional Pong, es lo primero que se va a ejecutar
-		juego = Juego("Double", 1)
+		juego = Juego("Double", 1, "humano")
 		juego.jugar()
 
-def Double_nivel2():
+def Double_cpu():
 	if __name__ == "__main__":#cuando le diga ejecutar, que llame primero al condicional Pong, es lo primero que se va a ejecutar
-		juego2 = Juego("Double", 2)
+		juego2 = Juego("Double", 1, "cpu")
 		juego2.jugar()
 
-def Double_nivel3():
-	if __name__ == "__main__":#cuando le diga ejecutar, que llame primero al condicional Pong, es lo primero que se va a ejecutar
-		juego3 = Juego("Double", 3)
-		juego3.jugar()
 
 def about_window():
 	window.withdraw()
@@ -180,30 +169,17 @@ help_label.place(x = 130, y = 310)
 
 
 #botones de inicio de juego
-oneplayer_button = Button(window, image = racket1cpuicon, command = Single_nivel1)
+oneplayer_button = Button(window, image = racket1cpuicon, command = Single_cpu)
 oneplayer_button.place(x = 300, y = 100)
-twoplayer_button = Button(window, image = racket12picon)
+twoplayer_button = Button(window, image = racket12picon, command = Single_humano)
 twoplayer_button.place(x = 300, y = 275)
-oneplayer2r_button = Button(window, image = racket2cpuicon)
+oneplayer2r_button = Button(window, image = racket2cpuicon, command = Double_cpu)
 oneplayer2r_button.place(x = 550, y = 100)
-twoplayer2r_button = Button(window, image = racket22picon)
+twoplayer2r_button = Button(window, image = racket22picon, command = Double_humano)
 twoplayer2r_button.place(x = 550, y= 275)
 about_button = Button(window, image = abouticon, command = about_window)
 about_button.place(x = 50, y = 400)
 help_button = Button(window, image = helpicon, command = help_window)
 help_button.place(x = 50, y = 300)
-
-
-boton2 = Button (window, text = "Single_Nivel2", font = ("arial", 12), width = 12, command = Single_nivel2)
-boton2.place (x = 250, y = 400)
-boton3 = Button (window, text = "Single_Nivel3", font = ("arial", 12), width = 12, command = Single_nivel3)
-boton3.place (x = 400, y = 400)
-boton4 = Button (window, text = "Double_Nivel1", font = ("arial", 12), width = 12, command = Double_nivel1)
-boton4.place (x = 100, y = 450)
-boton5 = Button (window, text = "Double_Nivel2", font = ("arial", 12), width = 12, command = Double_nivel2)
-boton5.place (x = 250, y = 450)
-boton6 = Button (window, text = "Double_Nivel3", font = ("arial", 12), width = 12, command = Double_nivel3)
-boton6.place (x = 400, y = 450)
-
 
 mainloop()
--- juego.py
+++ juego.py
@@ -36,7 +36,7 @@ TAMAÑO_BARRA_2 = 6
 TAMAÑO_BARRA_3 = 3
 
 class Juego:
-	def __init__(self, modo, nivel):
+	def __init__(self, modo, nivel, versus):
 		pygame.init()
 		self.pantalla = pygame.display.set_mode((ANCHO,LARGO))
 		pygame.display.set_caption("Pong")
@@ -48,30 +48,58 @@ class Juego:
 		self.bola = Bola(20,12, random.randrange(-1, 2), True)
 		self.nivel = nivel
 		self.modo = modo
+		self.versus = versus
+		self.CPU = 0
 		if self.nivel == 1:
 			self.tiempo = TIEMPO_NIVEL1
 			if self.modo == "Single":
 				self.barra1 = Barra(1,2,TAMAÑO_BARRA_1) 
 				self.barra2 = Barra(38,2,TAMAÑO_BARRA_1)
+				if self.versus == "humano":
+					self.CPU = 0
+				elif self.versus == "cpu":
+					self.CPU = 1
+				print(self.CPU)
+
 			else:
 				self.barra1 = Barra_doble(1,3,9,13,TAMAÑO_BARRA_1)
 				self.barra2 = Barra_doble(38,12,30,3,TAMAÑO_BARRA_1)
+				if self.versus == "humano":
+					self.CPU = 0
+				elif self.versus == "cpu":
+					self.CPU = 1
 		elif self.nivel == 2:
 			self.tiempo = TIEMPO_NIVEL2
 			if self.modo == "Single":
 				self.barra1 = Barra(1,2,TAMAÑO_BARRA_2) 
 				self.barra2 = Barra(38,2,TAMAÑO_BARRA_2)
+				if self.versus == "humano":
+					self.CPU = 0
+				elif self.versus == "cpu":
+					self.CPU = 1
 			else:
 				self.barra1 = Barra_doble(1,3,9,13,TAMAÑO_BARRA_2)
 				self.barra2 = Barra_doble(38,12,30,3,TAMAÑO_BARRA_2)
+				if self.versus == "humano":
+					self.CPU = 0
+				elif self.versus == "cpu":
+					self.CPU = 1
 		elif self.nivel == 3:
 			self.tiempo = TIEMPO_NIVEL3
 			if self.modo == "Single":
 				self.barra1 = Barra(1,2,TAMAÑO_BARRA_3) 
 				self.barra2 = Barra(38,2,TAMAÑO_BARRA_3)
+				if self.versus == "humano":
+					self.CPU = 0
+				elif self.versus == "cpu":
+					self.CPU = 1
 			else:
 				self.barra1 = Barra_doble(1,3,9,13,TAMAÑO_BARRA_3)
 				self.barra2 = Barra_doble(38,12,30,3,TAMAÑO_BARRA_3)
+				if self.versus == "humano":
+					self.CPU = 0
+				elif self.versus == "cpu":
+					self.CPU = 1
 
 	def crearMatriz(self):
 		for i in range(self.FILAS):
@@ -92,7 +120,7 @@ class Juego:
 		pygame.draw.line(self.pantalla, WHITE, [ANCHO//2, 20], [ANCHO//2,LARGO], 4)
 
 	def cpu(self):
-		if self.bola.get_x() > 15:
+		if self.bola.get_x() > 28:
 			if self.bola.get_y() > self.barra2.get_y():
 				self.barra2.mover(-1, self.matriz)
 			else:
@@ -160,7 +188,9 @@ class Juego:
 			self.dibujarMatriz()
 
 			self.dibujar()
-			self.cpu()
+
+			if self.CPU == 1:
+				self.cpu()
 
 			
 	def dibujar(self):
